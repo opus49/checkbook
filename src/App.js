6efe4da1dc360add3fc3671072cebe8a89d6f1b7
opus49 <puskar49@gmail.com>
2024-02-02 11:34:35 -0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [activeTab, setActiveTab] = useState("ledger");
+  const openTab = (tabName) => {
+    setActiveTab(tabName);
+  }
+
+
+       <button
+        className={`tab-button ${activeTab === 'ledger' ? 'active' : ''}`}
+        onClick={() => openTab('ledger')}
+      ></button>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <div className="tab-container">
+        <NonClickableButton />
+        <button className={`tab ${activeTab === 'ledger' ? 'active' : ''}`} onClick={() => openTab('ledger')}>Ledger</button>
+        <button className={`tab ${activeTab === 'credits' ? 'active' : ''}`} onClick={() => openTab('credits')}>Credits</button>
+        <button className={`tab ${activeTab === 'debits' ? 'active' : ''}`} onClick={() => openTab('debits')}>Debits</button>
+        <button className={`tab ${activeTab === 'budget' ? 'active' : ''}`} onClick={() => openTab('budget')}>Budget</button>
+        <NonClickableButton />
+      </div>
+
+      <div className="content">
+        <div id="ledger" className={`tab-content ${activeTab === 'ledger'? 'active' : ''}`}>
+          <p>Ledger...</p>
+        </div>
+
+        <div id="credits" className={`tab-content ${activeTab === 'credits'? 'active' : ''}`}>
+          <p>Credits...</p>
+        </div>
+
+        <div id="debits" className={`tab-content ${activeTab === 'debits'? 'active' : ''}`}>
+          <p>Debits...</p>
+        </div>
+
+        <div id="budget" className={`tab-content ${activeTab === 'budget'? 'active' : ''}`}>
+          <p>Budget...</p>
+        </div>
+      </div>
+
     </div>
+  );
+}
+
+function NonClickableButton() {
+  return (
+    <button className="tab non-clickable"></button>
   );
 }
 
